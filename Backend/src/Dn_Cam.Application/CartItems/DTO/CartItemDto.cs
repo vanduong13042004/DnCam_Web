0289d808586a390e54abfe6bf936227849c381e3
DESKTOP-9PHN3IF\migviet03 <nguyenvanduong130404@gmail.com>
2026-04-18 17:27:58 +0700

    bỏ các trường thừa của cartitemdto

--- Backend/src/Dn_Cam.Application/CartItems/DTO/CartItemDto.cs
+++ Backend/src/Dn_Cam.Application/CartItems/DTO/CartItemDto.cs
@@ -4,17 +4,17 @@ using Dn_Cam.Entities;
 
 namespace Dn_Cam.CartItems.DTO
 {
-    [AutoMapFrom(typeof(CartItem))]
+    [AutoMap(typeof(CartItem))]
     public class CartItemDto : EntityDto<int>
 
     {
         public int CartId { get; set; }
         public int ProductId { get; set; }
         public int Quantity { get; set; }
-        public string ProductName { get; set; }
-        public decimal ProductPrice { get; set; }
-        public string MainImage { get; set; }
-        public int StockQuantity { get; set; }
+        //public string ProductName { get; set; }
+        //public decimal ProductPrice { get; set; }
+        //public string MainImage { get; set; }
+        //public int StockQuantity { get; set; }
 
     }
 }
